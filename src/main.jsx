34595341133530eb6bf191954b2399c7c5a7c974
lamis2004 <lamisbadri579@gmail.com>
2025-04-21 13:22:28 +0200

    Switch to HashRouter for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import './global.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; 
+import App from './App.jsx';
+import './index.css';
+import './global.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+   
   plugins: [react()],
+  base: '/SakerOthmen',
   server: {
-    host: true, // This allows access from your local network
-  }
+    host: true,
+  },
 })
-
-
